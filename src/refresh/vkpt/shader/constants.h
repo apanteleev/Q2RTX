5bfcbad187283859c1d8775a05d1167513904f33
Alexey Panteleev <alpanteleev@nvidia.com>
2021-08-25 14:32:20 -0700

    Organized AS instance flags.

--- src/refresh/vkpt/shader/constants.h
+++ src/refresh/vkpt/shader/constants.h
@@ -113,12 +113,11 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #define AS_FLAG_OPAQUE          (1 << 0)
 #define AS_FLAG_TRANSPARENT     (1 << 1)
-#define AS_FLAG_PARTICLES       (1 << 2)
+#define AS_FLAG_EFFECTS         (1 << 2)
 #define AS_FLAG_VIEWER_MODELS   (1 << 3)
 #define AS_FLAG_VIEWER_WEAPON   (1 << 4)
-#define AS_FLAG_EXPLOSIONS      (1 << 5)
-#define AS_FLAG_SKY             (1 << 6)
-#define AS_FLAG_CUSTOM_SKY      (1 << 7)
+#define AS_FLAG_SKY             (1 << 5)
+#define AS_FLAG_LIGHTS          (1 << 6)
 #define AS_FLAG_EVERYTHING      0xFF
 
 #define AS_INSTANCE_FLAG_DYNAMIC        (1 << 23)
